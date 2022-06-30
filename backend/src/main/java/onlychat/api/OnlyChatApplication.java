a2375b87fa285816e55d7b371bcebb205c0114e0
Lucas Soares <soares0lucas8@gmail.com>
2022-06-29 21:15:45 -0300

    estrutural inicial do projeto

---
+++ backend/src/main/java/onlychat/api/OnlyChatApplication.java
@@ -0,0 +1,13 @@
+package onlychat.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyChatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/onlychat/api/OnlyChatApplicationTests.java
@@ -0,0 +1,13 @@
+package onlychat.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
